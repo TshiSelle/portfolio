fe9b0b4f08eb2e325c3a28445cc04ab229f7ed93
OmarHoussami <omar.houssami20@gmail.com>
2022-04-01 13:26:52 +0300

    Contact modifications

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,11 +12,23 @@ const Footer = () => {
       <ul className='permalinks'>
         <li>
           <a href='#'>Home</a>
+        </li>
+        <li>
           <a href='#about'>About</a>
+        </li>
+        <li>
           <a href='#experience'>Experience</a>
+        </li>
+        <li>
           <a href='#field'>Field</a>
+        </li>
+        <li>
           <a href='#portfolio'>Portfolio</a>
+        </li>
+        <li>
           <a href='#testimonials'>Testimonials</a>
+        </li>
+        <li>
           <a href='#contact'>Contact</a>
         </li>
       </ul>
