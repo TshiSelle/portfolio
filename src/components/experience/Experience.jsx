5866c790a1f591021352d198908218f2d32559bd
OmarHoussami <omar.houssami20@gmail.com>
2022-04-01 14:14:30 +0300

    text edits

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -35,7 +35,7 @@ const Experience = () => {
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4>Bootsrtap</h4>
+                <h4>Bootstrap</h4>
                 <small className='text-light'>Intermediate</small>
               </div>
             </article>
